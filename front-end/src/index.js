82ab96c93d0739e9e0a9305c4460391e8656ae99
unknown <mohaiminislam94@gmail.com>
2019-08-25 04:33:36 +0600

    prepare for gh-pages deployment

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
